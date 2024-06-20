7258bdc44927cf2c85e525d33493dcddde93e731
Liz Ainslie <lizainslie16@gmail.com>
2024-06-20 10:47:33 -0700

    chore(lib,example): fix unwrap

--- crates/server/src/node.rs
+++ crates/server/src/node.rs
@@ -88,7 +88,7 @@ impl OSProtocolNode {
 
     pub fn test_outbound(&self, url: OSPUrl) {
         info!("Testing outbound connection to {url}");
-        let mut conn = OutboundConnection::create(url, self.private_key.clone(), self.hostname.clone())?;
+        let mut conn = OutboundConnection::create(url, self.private_key.clone(), self.hostname.clone()).unwrap();
         conn.begin().unwrap();
     }
 }
